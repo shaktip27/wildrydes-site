1b9271a06a6e8d4ea7857290fcb8d7697895e21c
shakti <shakti.patel@mindinventory.com>
2025-05-02 16:10:09 +0530

    reverted ride url

--- js/ride.js
+++ js/ride.js
@@ -18,7 +18,7 @@ WildRydes.map = WildRydes.map || {};
     function requestUnicorn(pickupLocation) {
         $.ajax({
             method: 'POST',
-            url: _config.api.invokeUrl + '/dev/ride',
+            url: _config.api.invokeUrl + '/ride',
             headers: {
                 Authorization: authToken
             },
